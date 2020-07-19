2aa9340526d4540e7a670421c57f072853916bd2
Chansik Moon <chansik.moon94@gmail.com>
2020-07-19 00:37:55 -0500

    Added Kahn's algorithm method.

--- Medium/210_Course_Scheduler_II.cs
+++ Medium/210_Course_Scheduler_II.cs
@@ -1,5 +1,47 @@
 public class Solution {
     public int[] FindOrder(int n, int[][] prs) {
+        return TopologicalSorting(n, prs);
+        // return KahnTopologicalSorting(n, prs);
+    }
+
+    private int[] KahnTopologicalSorting(int n ,int[][] prs)
+    {
+        List<int>[] graph = new List<int>[n];
+        List<int> retval = new List<int>();
+        int[] indegree = new int[n];
+        Queue<int> q = new Queue<int>();
+        
+        for (int i = 0; i < n; i++)
+            graph[i] = new List<int>();
+        
+        foreach (var pr in prs)
+        {
+            graph[pr[1]].Add(pr[0]);
+            indegree[pr[0]]++;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (indegree[i] == 0)
+                q.Enqueue(i);
+        }
+
+        while (q.Count > 0)
+        {
+            int pre = q.Dequeue();
+            retval.Add(pre);
+
+            for (int i = 0; i < graph[pre].Count; i++)
+            {
+                if (--indegree[graph[pre][i]] == 0)
+                    q.Enqueue(graph[pre][i]);
+            }
+        }
+
+        return retval.Count == n ? retval.ToArray() : new int[0];
+    }
+
+    public int[] TopologicalSorting(int n, int[][] prs) {
         List<int>[] graph = new List<int>[n];
         List<int> retval = new List<int>();
         int[] visited = new int[n];
@@ -12,7 +54,7 @@ public class Solution {
         
         for (int i = 0; i < n; i++)
             if (!Topo(graph, visited, retval, i))
-                return new int[n];
+                return new int[0];
         
         retval.Reverse();
         
