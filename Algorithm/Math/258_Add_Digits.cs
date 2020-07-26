644c2ce13dd7315e3ef298fd98b87a96268c503c
Moon <Chansik.Moon@Emerson.com>
2020-07-26 07:19:53 -0500

    Added 258.Add Digits

---
+++ Algorithm/Math/258_Add_Digits.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int AddDigits(int num) {    
+        return DigitalRoot(num);
+    }
+    
+    private int DigitalRoot(int num)
+    {
+        // num == 18
+        // 17 % 9 => 8 + 1 = 9
+        return num == 0 ? 0 : 1 + (num - 1) % 9;
+    }
+    
+    private int BruteForce(int num)
+    {
+        while (num > 9)   
+        {
+            int sum = 0;
+            
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            
+            num = sum;
+        }
+        
+        return num;
+    }
+}
