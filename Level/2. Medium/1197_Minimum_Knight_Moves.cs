800d234f85c7a99dd5e899d88ee363cc846f403a
Chansik Moon <chansik.moon94@gmail.com>
2020-07-29 23:03:24 -0500

    Added more base cases and a reference link

--- Level/2. Medium/1197_Minimum_Knight_Moves.cs
+++ Level/2. Medium/1197_Minimum_Knight_Moves.cs
@@ -12,10 +12,16 @@ public class Solution {
         if (memo.ContainsKey(key))
             return memo[key];
         
+        // These base cases are from here:
+        // https://math.stackexchange.com/questions/1135683/minimum-number-of-steps-for-knight-in-chess
         if (x + y == 0)
             return 0;
+        else if (x + y == 1)
+            return 3;
         else if (x + y == 2)
             return 2;
+        else if (x == 2 && y == 2)
+            return 4;
         
         int min = Math.Min(Helper(Math.Abs(x - 1), Math.Abs(y - 2), memo), 
                            Helper(Math.Abs(x - 2), Math.Abs(y - 1), memo)) + 1;
