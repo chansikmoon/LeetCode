4059b6873e5d09cfce29ae37a10561243262ad06
Chansik Moon <chansik.moon94@gmail.com>
2020-07-31 20:13:27 -0500

    Added 70.Climbing Stairs

---
+++ Algorithm/DynamicProgramming/70_Climbing_Stairs.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n < 3)
+            return n;
+        
+        int first = 1, second = 2;
+        
+        for (int i = 3; i <= n; i++)
+        {
+            int tmp = first;
+            first = second;
+            second = tmp + first;
+        }
+        
+        return second;
+    }
+}
