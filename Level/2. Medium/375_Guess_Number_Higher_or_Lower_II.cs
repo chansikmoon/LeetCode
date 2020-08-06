2db1650d2231db649c8b609f104db5f1bba29913
Chansik Moon <chansik.moon94@gmail.com>
2020-08-05 23:46:47 -0500

    Added idea for 375

--- Level/2. Medium/375_Guess_Number_Higher_or_Lower_II.cs
+++ Level/2. Medium/375_Guess_Number_Higher_or_Lower_II.cs
@@ -1,3 +1,7 @@
+// Idea: Given n, make a guess k. Then break the [1, n] interval into [1, k -1] and [k + 1, n] to find the min of worst case cost.
+
+// i <= k <= j
+// dp[i,j] = Math.Min(dp[i,j], k + Math.Max(dp[i, k - 1], dp[k + 1, j]);
 public class Solution {
     public int GetMoneyAmount(int n) {
         int[,] dp = new int[n + 1, n + 1];
