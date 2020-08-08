dea47c0c7cdfcb7661383d212dbaa604f2fa52d8
Chansik Moon <chansik.moon94@gmail.com>
2020-08-07 20:15:17 -0500

    Added 987.Vertical Order Traversal of a Binary Tree

---
+++ Level/2. Medium/987_Vertical_Order_Traversal_of_a_Binary_Tree.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> VerticalTraversal(TreeNode root) {
+        Dictionary<int, Dictionary<int,List<int>>> map = new Dictionary<int, Dictionary<int,List<int>>>();
+        
+        Traverse(root, map, 0, 0);
+        
+        List<IList<int>> ans = new List<IList<int>>();
+
+        foreach (var kvp in map.OrderBy(x => x.Key))
+        {
+            ans.Add(new List<int>(kvp.Value.ToList()));
+        }
+
+        return ans;
+    }
+    
+    private void Traverse(TreeNode root, Dictionary<int, Dictionary<int, List<int>>> map, int x, int y)
+    {
+        if (root == null) return;
+        
+        if (!map.ContainsKey(x))
+            map.Add(x, new Dictionary<int, List<int>>());
+        
+        if (!map[x].ContainsKey(y))
+            map[x].Add(y, new List<int>());
+        
+        map[x][y].Add(root.val);
+        
+        Traverse(root.left, map, x - 1, y + 1);
+        Traverse(root.right, map, x + 1, y + 1);
+    }
+}
