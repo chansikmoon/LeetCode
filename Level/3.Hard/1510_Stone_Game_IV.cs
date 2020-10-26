20cdeb54c9d708d89296455f013b32c5b00be401
Chansik Moon <chansik.moon94@gmail.com>
2020-10-25 21:29:31 -0500

    Added 1510.Stone Game IV

---
+++ Level/3.Hard/1510_Stone_Game_IV.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    // O(n^1.5)
+    // O(n)
+    public bool WinnerSquareGame(int n) {
+        bool[] dp = new bool[n + 1];
+        
+        // O(n)
+        for (int i = 1; i <= n; i++)
+        {
+            // O(sqrt(n))
+            for (int k = 1; k * k <= i; k++)
+            {
+                // if dp[i - k * k] is false, then dp[i] should be true
+                // if dp[i - k * k] is true, then Alice loses the game at ith turn.
+                // Ex) dp[1 - (1)] == false, then dp[1] = true
+                if (!dp[i - k * k])
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
