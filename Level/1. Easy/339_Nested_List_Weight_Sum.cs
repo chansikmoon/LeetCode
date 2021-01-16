dbda17de2e5c01a75fc12c05396601ae373fdb70
Chansik Moon <chansik.moon94@gmail.com>
2021-01-15 22:23:50 -0600

    Added 339.Nested List Weight Sum

---
+++ Level/1. Easy/339_Nested_List_Weight_Sum.cs
@@ -0,0 +1,48 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * interface NestedInteger {
+ *
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool IsInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     int GetInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void SetInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void Add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     IList<NestedInteger> GetList();
+ * }
+ */
+public class Solution {
+    public int DepthSum(IList<NestedInteger> nestedList) {
+        return DFS(nestedList, 1);
+    }
+    
+    public int DFS(IList<NestedInteger> nestedList, int depth)
+    {
+        int ret = 0;
+        
+        foreach (var nl in nestedList)
+        {
+            ret += nl.IsInteger() ? 
+                nl.GetInteger() * depth : 
+                DFS(nl.GetList(), depth+1);
+        }
+        
+        return ret;
+    }
+}
