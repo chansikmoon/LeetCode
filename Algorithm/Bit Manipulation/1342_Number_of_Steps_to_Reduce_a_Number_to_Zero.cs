649b509ea43e33d2c9c414052d05df5f25f667fa
Chansik Moon <chansik.moon94@gmail.com>
2021-02-13 18:43:21 -0600

    Added 1342.Number of Steps to Reduce a Number to Zero

--- Algorithm/Bit Manipulation/1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.cs
+++ Algorithm/Bit Manipulation/1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.cs
@@ -2,7 +2,18 @@ public class Solution {
     public int NumberOfSteps (int num) {
         if (num == 0)
             return 0;
+            
+        int ret = 0;
+        while (num > 0)
+        {
+            ret += (num & 1) > 0 ? 2 : 1;
+            num >>= 1;
+        }
         
+        return ret - 1;
+    }
+
+    private int BruteForce(int num) {
         int maxBitPlace = 0, numOfOnes = 0;
         
         for (int i = 0; i < 32; i++)
