43fd43c93ea7dc96c926b7bfbc4ffe0f3614e528
Chansik Moon <chansik.moon94@gmail.com>
2021-02-19 22:03:15 -0600

    Added 1249.Minimum Remove to Make Valid Parentheses

---
+++ Algorithm/Stack/1249_Minimum_Remove_to_Make_Valid_Parentheses.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public string MinRemoveToMakeValid(string s) {
+        var setOfIndexToRemove = new HashSet<int>();
+        var st = new Stack<int>();
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(')
+            {
+                st.Push(i);
+            }
+            else if (s[i] == ')')
+            {
+                if (st.Count == 0)
+                {
+                    setOfIndexToRemove.Add(i);
+                    continue;
+                }
+                
+                st.Pop();
+            }
+        }
+        
+        while (st.Count > 0)
+            setOfIndexToRemove.Add(st.Pop());
+        
+        var sb = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (setOfIndexToRemove.Contains(i))
+                continue;
+            
+            sb.Append(s[i]);
+        }
+        
+        return sb.ToString();
+    }
+}
