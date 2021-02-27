b086c0663f04d27c728b1f95803d6608e63e40e8
Chansik Moon <chansik.moon94@gmail.com>
2021-02-26 23:11:10 -0600

    Added 946.Validate Stack Sequences

---
+++ Level/2. Medium/946_Validate_Stack_Sequences.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public bool ValidateStackSequences(int[] pushed, int[] popped) {
+        int push = 0, pop = 0;
+        
+        for (int i = 0; i < pushed.Length; i++)
+        {
+            pushed[push++] = pushed[i];
+            
+            while (push > 0 && pushed[push-1] == popped[pop])
+            {
+                push--;
+                pop++;
+            }
+        }
+        
+        return push == 0;
+    }
+    
+    private bool BruteForce(int[] pushed, int[] popped)
+    {
+        var st = new Stack<int>();
+
+        for (int i = 0, j = 0; i < pushed.Length; i++)
+        {
+            st.Push(pushed[i]);
+            
+            while (st.Count > 0 && st.Peek() == popped[j])
+            {
+                st.Pop();
+                j++;
+            }
+        }
+            
+        return st.Count == 0;
+    }
+}
