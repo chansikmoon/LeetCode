3ff63dd55c6542d52b0fce07950d03be5de69beb
Chansik Moon <chansik.moon94@gmail.com>
2021-03-06 18:19:24 -0600

    Added another solution for 820.Short Encoding of Words

--- Level/2. Medium/820_Short_Encoding_of_Words.cs
+++ Level/2. Medium/820_Short_Encoding_of_Words.cs
@@ -18,4 +18,43 @@ public class Solution {
         
         return ret;
     }
+
+    public int MinimumLengthEncoding2(string[] words) {
+        var list = new List<string>();
+        
+        for (int i = 0; i < words.Length; i++)
+        {
+            var charArr = words[i].ToCharArray();
+            Array.Reverse(charArr);
+            list.Add(new string(charArr));
+            Console.WriteLine(new string(charArr));
+        }
+        
+        var sorted = list.OrderBy(w => w).ToList();
+        
+        foreach (var a in sorted)
+            Console.WriteLine(a);
+        
+        int ret = 0;
+        
+        /*
+            ["time", "me", "bell"]
+            em
+            emit
+            lleb
+
+            ["p","grah","qwosp"]
+            harg
+            p
+            psowq
+        */
+
+        // Math.Min() for "harg" and the next string is "p". Need to use min length.
+        for (int i = 0; i < sorted.Count -1; i++)
+        {
+            ret += sorted[i] == sorted[i+1].Substring(0, Math.Min(sorted[i].Length, sorted[i+1].Length)) ? 0 : sorted[i].Length+1;
+        }
+        
+        return ret + sorted[sorted.Count-1].Length + 1;
+    }
 }
