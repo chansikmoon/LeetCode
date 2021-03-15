53b8fb8148d630e25927fa95cc95b4590e1302b9
Chansik Moon <41027720+chansikmoon@users.noreply.github.com>
2021-03-14 23:56:52 -0500

    Added 1650.Lowest Common Ancestor of a Binary Tree III

---
+++ Algorithm/Cycling/1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III.cs
@@ -0,0 +1,25 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+*/
+
+public class Solution
+{
+    public Node LowestCommonAncestor(Node p, Node q)
+    {
+        Node a = p, b = q;
+
+        while (a != b)
+        {
+            a = a == null ? q : a.parent;
+            b = b == null ? p : b.parent;
+        }
+
+        return a;
+    }
+}
