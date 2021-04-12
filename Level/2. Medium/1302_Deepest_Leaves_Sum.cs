063386c3eda9421aa50578d96cc34eabbe19908c
Chansik Moon <41027720+chansikmoon@users.noreply.github.com>
2021-04-11 20:45:42 -0500

    Added 1302.Deepest Leaves Sum

---
+++ Level/2. Medium/1302_Deepest_Leaves_Sum.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public int DeepestLeavesSum(TreeNode root)
+    {
+        int targetDepth = GetDeepestDepth(root);
+        return GetSumOfDeepestDepth(root, 1, targetDepth);
+    }
+
+    private int GetDeepestDepth(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        return Math.Max(GetDeepestDepth(root.left), GetDeepestDepth(root.right)) + 1;
+    }
+
+    private int GetSumOfDeepestDepth(TreeNode root, int depth, int target)
+    {
+        if (root == null)
+            return 0;
+
+        if (depth == target)
+            return root.val;
+
+        return GetSumOfDeepestDepth(root.left, depth + 1, target) +
+            GetSumOfDeepestDepth(root.right, depth + 1, target);
+    }
+}
