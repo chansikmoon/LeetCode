a05c861f647eebe417d79097a11d92e28123bdf3
Chansik Moon <chansik.moon94@gmail.com>
2021-11-13 20:13:03 -0600

    Updated 1286.Iterator for Combination

--- Algorithm/Backtracking/1286_Iterator_for_Combination.cs
+++ Algorithm/Backtracking/1286_Iterator_for_Combination.cs
@@ -1,20 +1,25 @@
 public class CombinationIterator {
-    private Queue<string> q = new Queue<string>();
-    
+    private Queue<string> q { get; set; }
+    private int k { get; set; }
     public CombinationIterator(string characters, int combinationLength) {
-        GenerateCombination(characters, "", 0, combinationLength, characters.Length);            
+        q = new Queue<string>();
+        k = combinationLength;
+        
+        GenerateCombination(characters, "", 0);
     }
     
-    private void GenerateCombination(string str, string s, int index, int k, int n)
+    private void GenerateCombination(string str, string s, int index)
     {
-        if (s.Length == k)
+        int n = str.Length;
+        
+        if (s.Length >= k)
         {
             q.Enqueue(s);
             return;
         }
         
         for (int i = index; i < n; i++)
-            GenerateCombination(str, s + str[i], i + 1, k, n);
+            GenerateCombination(str, s + str[i], i + 1);
     }
     
     public string Next() {
