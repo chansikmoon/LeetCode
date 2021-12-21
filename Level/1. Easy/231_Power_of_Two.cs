0bd7ec51f69d57eab3315dd8719f1af5a8166593
Chansik Moon <chansik.moon94@gmail.com>
2021-12-20 23:00:19 -0600

    Added 231.Power of Two

--- Level/1. Easy/231_Power_of_Two.cs
+++ Level/1. Easy/231_Power_of_Two.cs
@@ -1,5 +1,10 @@
 public class Solution {
     public bool IsPowerOfTwo(int n) {
+        // -n = ~n + 1
         return n > 0 && (n & -n) == n;
+    }
+
+    public bool IsPowerOfTwo1(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
