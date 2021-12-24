544db38e824334886726fa7f065079aba04273e7
Chansik Moon <chansik.moon94@gmail.com>
2021-12-23 23:23:14 -0600

    Rewrote 56.Merge Intervals

--- Algorithm/Array/56_Merge_Intervals.cs
+++ Algorithm/Array/56_Merge_Intervals.cs
@@ -1,19 +1,13 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        Array.Sort(intervals, (a ,b) => a[0] - b[0]);
-        
-        List<int[]> ret = new List<int[]>();
-        
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+        var ret = new List<int[]>();
         ret.Add(intervals[0]);
         
         for (int i = 1; i < intervals.Length; i++)
         {
-            var last = ret.Last();
-            
-            if (last[0] <= intervals[i][0] && last[1] >= intervals[i][1])
-                continue;
-            else if (last[1] >= intervals[i][0])
-                last[1] = intervals[i][1];
+            if (intervals[i][0] <= ret.Last()[1])
+                ret.Last()[1] = Math.Max(ret.Last()[1], intervals[i][1]);
             else
                 ret.Add(intervals[i]);
         }
