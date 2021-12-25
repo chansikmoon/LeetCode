d8304a8c85f341be69b7a292c00e5a3e26cbb04e
Chansik Moon <chansikmoon@Chansiks-MacBook-Pro.local>
2021-12-24 18:39:22 -0600

    Rewrote 227.Basic Calculator II

--- Algorithm/Stack/227_Basic_Calculator_II.cs
+++ Algorithm/Stack/227_Basic_Calculator_II.cs
@@ -1,36 +1,32 @@
 public class Solution {
     public int Calculate(string s) {
-        Stack<int> nums = new Stack<int>();
+        var numSt = new Stack<int>();
         int num = 0;
         char sign = '+';
         
         for (int i = 0; i < s.Length; i++)
         {
             if (char.IsDigit(s[i]))
+            {
                 num = num * 10 + (s[i] - '0');
+            }
             
             if ((!char.IsWhiteSpace(s[i]) && !char.IsDigit(s[i])) || i == s.Length - 1)
             {
                 if (sign == '+')
-                    nums.Push(num);
+                    numSt.Push(num);
                 else if (sign == '-')
-                    nums.Push(-num);
+                    numSt.Push(-num);
                 else if (sign == '*')
-                    nums.Push(nums.Pop() * num);
+                    numSt.Push(numSt.Pop() * num);
                 else if (sign == '/')
-                    nums.Push(nums.Pop() / num);
+                    numSt.Push(numSt.Pop() / num);
                 
-                sign = s[i];
                 num = 0;
+                sign = s[i];
             }
         }
         
-        int ret = 0;
-        foreach (int n in nums)
-        {
-            ret += n;
-        }
-        
-        return ret;
+        return numSt.Sum();
     }
 }
