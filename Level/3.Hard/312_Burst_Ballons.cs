54e99f1774ff496390427b15bf5d8217dc159b38
Chansik Moon <chansik.moon94@gmail.com>
2021-12-31 20:17:27 -0600

    Revisited 312.Burst Ballons

--- Level/3.Hard/312_Burst_Ballons.cs
+++ Level/3.Hard/312_Burst_Ballons.cs
@@ -1,27 +1,36 @@
 public class Solution {
     public int MaxCoins(int[] nums) {
-        int[] arr = new int[nums.Length + 2];
-        int index = 1;
+        var ballons = new int[nums.Length + 2];
+        int length = 1;
+        
         foreach (int num in nums)
-            if (num > 0)
-                arr[index++] = num;
-        arr[0] = 1;
-        arr[index++] = 1;
-            
-        int[,] dp = new int[index, index];
-        for (int k = 2; k < index; k++)
         {
-            for (int left = 0; left < index - k; left++)
+            if (num > 0)
+            {
+                ballons[length++] = num;
+            }
+        }
+        
+        ballons[0] = 1;
+        ballons[length++] = 1;
+        
+        var dp = new int[length, length];
+        
+        for (int k = 2; k < length; k++)
+        {
+            for (int left = 0; left < length - k; left++)
             {
                 int right = left + k;
-                for (int i = left + 1; i < right; i++)
+                
+                for (int middle = left + 1; middle < right; middle++)
                 {
                     dp[left, right] = Math.Max(dp[left, right], 
-                                              arr[left] * arr[i] * arr[right] + dp[left,i] + dp[i,right]);
+                                               ballons[left] * ballons[middle] * ballons[right] + 
+                                               dp[left, middle] + dp[middle, right]);
                 }
             }
         }
         
-        return dp[0, index - 1];
+        return dp[0, length - 1];
     }
 }
