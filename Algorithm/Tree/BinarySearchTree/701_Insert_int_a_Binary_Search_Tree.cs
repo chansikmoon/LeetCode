4c7fc685910c0110f6bca355ad4b8a4606a06e76
Chansik Moon <chansik.moon94@gmail.com>
2022-01-11 22:27:16 -0600

    Revisited 701.Insert Into a Binary Search Tree

--- Algorithm/Tree/BinarySearchTree/701_Insert_int_a_Binary_Search_Tree.cs
+++ Algorithm/Tree/BinarySearchTree/701_Insert_int_a_Binary_Search_Tree.cs
@@ -13,10 +13,14 @@
  */
 public class Solution {
     public TreeNode InsertIntoBST(TreeNode root, int val) {
-        if (root == null) return new TreeNode(val);
+        if (root == null)
+            return new TreeNode(val);
         
-        if (root.val < val) root.right = InsertIntoBST(root.right, val);
-        else root.left = InsertIntoBST(root.left, val);
+        if (root.val < val)
+            root.right = InsertIntoBST(root.right, val);
+        else 
+            root.left = InsertIntoBST(root.left, val);
+        
         return root;
     }
     
