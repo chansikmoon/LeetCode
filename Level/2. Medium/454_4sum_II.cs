a6b1486edbfc9727c2a9cb25ccae8f686c87614e
Chansik Moon <chansik.moon94@gmail.com>
2022-02-02 21:25:18 -0600

    Added 454. 4Sum II

--- Level/2. Medium/454_4sum_II.cs
+++ Level/2. Medium/454_4sum_II.cs
@@ -1,28 +1,23 @@
 public class Solution {
-    public int FourSumCount(int[] A, int[] B, int[] C, int[] D) {
+    public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         var map = new Dictionary<int, int>();
         
-        for (int i = 0; i < C.Length; i++)
-        {
-            for (int j = 0; j < D.Length; j++)
-            {
-                int sum = C[i] + D[j];
+        foreach (int i in nums1) {
+            foreach (int j in nums2) {
+                int sum = i + j;
                 if (!map.ContainsKey(sum))
                     map.Add(sum, 0);
-                
                 map[sum]++;
             }
         }
         
         int ret = 0;
         
-        for (int i = 0; i < A.Length; i++)
-        {
-            for (int j = 0; j < B.Length; j++)
-            {
-                int sum = -A[i] - B[j];
-                if (map.ContainsKey(sum))
-                    ret += map[sum];
+        foreach (int i in nums3) {
+            foreach (int j in nums4) {
+                int neg = -i - j;
+                if (map.ContainsKey(neg))
+                    ret += map[neg];
             }
         }
         
