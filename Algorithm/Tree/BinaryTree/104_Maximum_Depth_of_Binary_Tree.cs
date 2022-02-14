62e38292691c6540b42063553de4bf7ce6c8e645
Chansik Moon <chansik.moon94@gmail.com>
2022-02-13 18:23:56 -0600

    Added 104.Maximum Depth of Binary Tree

--- Algorithm/Tree/BinaryTree/104_Maximum_Depth_of_Binary_Tree.cs
+++ Algorithm/Tree/BinaryTree/104_Maximum_Depth_of_Binary_Tree.cs
@@ -11,11 +11,12 @@
  *     }
  * }
  */
+ // Revisited 02/14/2022
 public class Solution {
     public int MaxDepth(TreeNode root) {
         if (root == null) 
             return 0;
-        
+
         return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
     }
 }
