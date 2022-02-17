01b8f4ced37b283b7773e8b2f9ac2f17a3714a76
Chansik Moon <chansik.moon94@gmail.com>
2022-02-16 21:13:31 -0600

    Added 39.Combination Sum

--- Level/2. Medium/39_Combination_Sum.cs
+++ Level/2. Medium/39_Combination_Sum.cs
@@ -1,20 +1,21 @@
 public class Solution {
     public IList<IList<int>> CombinationSum(int[] candidates, int target) {
-        List<IList<int>> ret = new List<IList<int>>();
+        var ret = new List<IList<int>>();
         Backtracking(ret, new List<int>(), candidates, target, 0);
         return ret;
     }
     
-    public void Backtracking(List<IList<int>> ret, List<int> list, int[] cand, int target, int index)
+    public void Backtracking(List<IList<int>> ret, List<int> list, int[] cand, int target, int i)
     {
-        if (list.Sum() >= target)
+        int sum = list.Sum();
+        if (sum >= target)
         {
-            if (list.Sum() == target)
+            if (sum == target)
                 ret.Add(new List<int>(list));
             return;
         }
         
-        for (int i = index; i < cand.Length; i++)
+        for (; i < cand.Length; i++)
         {
             list.Add(cand[i]);
             Backtracking(ret, list, cand, target, i);
