4cc8c860ee3b334b61d798d584e74c017a9f28db
Chansik Moon <chansik.moon94@gmail.com>
2022-02-27 22:55:23 -0600

    Added 228.Summary Ranges

--- Level/1. Easy/228_Summary_Ranges.cs
+++ Level/1. Easy/228_Summary_Ranges.cs
@@ -1,17 +1,13 @@
 public class Solution {
     public IList<string> SummaryRanges(int[] nums) {
-        List<string> ret = new List<string>();
+        var ret = new List<string>();
         
-        for (int l = 0, r = 0; r < nums.Length; r++)
-        {
-            if (r + 1 < nums.Length && nums[r] + 1 == nums[r + 1])
+        for (int l = 0, r = 0; r < nums.Length; r++) {
+            if (r + 1 < nums.Length && nums[r] + 1 == nums[r + 1]) {
                 continue;
+            }
             
-            if (l == r)
-                ret.Add(nums[r].ToString());
-            else
-                ret.Add(string.Format("{0}->{1}", nums[l], nums[r]));
-            
+            ret.Add(l == r ? $"{nums[r]}" : $"{nums[l]}->{nums[r]}");
             l = r + 1;
         }
         
