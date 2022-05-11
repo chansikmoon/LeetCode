6f93f5d0c96a62dcc5f6a5d783e29614db9baebd
Chansik Moon <chansik.moon94@gmail.com>
2022-05-11 03:15:38 -0500

    Added 1641.Count Sorted Vowel Strings

--- Level/2. Medium/1641_Count_Sorted_Vowel_Strings.cs
+++ Level/2. Medium/1641_Count_Sorted_Vowel_Strings.cs
@@ -12,4 +12,26 @@ public class Solution {
         
         return dp[5];
     }
+
+    // 05/11/22
+    // Brute force implementation
+    public int CountVowelStrings(int n) {
+        var dp = new int[6, n + 1];
+        
+        for (int i = 1; i <= 5; i++) {
+            dp[i, 1] = i; 
+        }
+        
+        for (int j = 1; j <= n; j++) {
+            dp[1, j] = 1;    
+        }   
+        
+        for (int i = 2; i <= 5; i++) {
+            for (int j = 2; j <= n; j++) {
+                dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+            }
+        }            
+        
+        return dp[5, n];
+    }
 }
