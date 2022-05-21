188fde8a186968165a2f7436047553069e758ed2
Chansik Moon <chansik.moon94@gmail.com>
2022-05-20 21:40:40 -0500

    Revisited 63.Unique Paths II

--- Algorithm/DynamicProgramming/63_Unique_Paths_II.cs
+++ Algorithm/DynamicProgramming/63_Unique_Paths_II.cs
@@ -2,24 +2,20 @@ public class Solution
 {
     public int UniquePathsWithObstacles(int[][] obstacleGrid)
     {
-        if (obstacleGrid.Length == 0)
-            return 0;
-
-        int n = obstacleGrid.Length, m = obstacleGrid[0].Length;
-        var dp = new int[n + 1, m + 1];
-        dp[0, 1] = 1;
-
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                if (obstacleGrid[i - 1][j - 1] == 0)
+        int m = obstacleGrid.Length;
+        int n = obstacleGrid[0].Length;
+        var dp = new int[m + 1, n + 1];
+        
+        dp[1, 0] = 1;
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (obstacleGrid[i-1][j-1] == 0) {
                     dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
-                else
-                    dp[i, j] = 0;
+                }
             }
         }
-
-        return dp[n, m];
+        
+        return dp[m, n];
     }
 }
